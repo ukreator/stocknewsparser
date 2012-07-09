cae3cd0b6604d0d1023607aec330bd67fec3e870
Dmitry Sobinov <sobinov@crystalnix.com>
2012-07-09 18:19:39 +0700

    extracted rss_item record

---
+++ src/snp_rss_item.hrl
@@ -0,0 +1,9 @@
+%%% -------------------------------------------------------------------
+%%% Author  : Dmitry Sobinov
+%%% Email: sobinov@crystalnix.com
+%%% Description : Record representing an RSS item.
+%%%
+%%% Created : 09.07.2012
+%%% -------------------------------------------------------------------
+
+-record(rss_item, {link, title, publish_date, guid}).
