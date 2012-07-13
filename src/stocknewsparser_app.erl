5fae582e4de7df505aba96d046c702753e42e41d
Dmitry Sobinov <sobinov@crystalnix.com>
2012-07-13 18:24:19 +0700

    added more options to config files

--- src/stocknewsparser_app.erl
+++ src/stocknewsparser_app.erl
@@ -15,10 +15,6 @@
 -include("snp_logging.hrl").
 
 
-%% List of RSS feeds we monitor
--define(RSS_FEEDS, ["http://finance.yahoo.com/news/category-economy-govt-and-policy/rss",
-					"http://finance.yahoo.com/news/category-earnings/rss"]).
-
 % how often RSS should be retrieved from a remote site, in seconds
 -define(RSS_FETCH_INTERVAL, 60).
 
@@ -29,9 +25,11 @@
 
 start(_StartType, _StartArgs) ->
 	?INFO("Starting stocknewsparser application", []),
+	{ok, FeedAddresses} = application:get_env(stocknewsparser, rss_feed_addresses),
+	{ok, RssFetchInterval} = application:get_env(stocknewsparser, rss_fetch_interval),
     Res = snp_sup:start_link(),
 	case Res of
-		{ok, _Pid} -> start_children();
+		{ok, _Pid} -> start_children(FeedAddresses, RssFetchInterval);
 		_ -> ok
 	end,
 	Res.
@@ -46,19 +44,17 @@ stop(_State) ->
 %% ====================================================================
 
 
-start_children() ->
-	FeedList = ?RSS_FEEDS,
-	%FeedList = [],
+start_children(FeedAddresses, RssFetchInterval) ->
 	% generate index for additional seeding factor (increase seconds in current time)
 	lists:foldl(
-			fun(Url, Index) -> start_child(Url, Index),
+			fun(Url, Index) -> start_child(Url, Index, RssFetchInterval),
 		 	Index + 1
 		 	end, 
-		0, FeedList),
+		0, FeedAddresses),
 	ok.
 
-start_child(Url, Index) ->
-	case snp_rss_downloader_server:create(Url, ?RSS_FETCH_INTERVAL, Index) of
+start_child(Url, Index, RssFetchInterval) ->
+	case snp_rss_downloader_server:create(Url, RssFetchInterval, Index) of
 		{error, Reason} -> ?ERROR("Failed to start child worker process ~p", [Reason]), ok;
 		_Other -> ok
 	end.
